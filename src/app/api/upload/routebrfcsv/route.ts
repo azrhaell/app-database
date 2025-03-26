db08c7d4f38c3a16201d4b955aa95c7d953dd840
azrhaell <marcelolobo27@gmail.com>
2025-03-26 15:54:57 -0300

    commit wrong 22

--- src/app/api/upload/routebrfcsv/route.ts
+++ src/app/api/upload/routebrfcsv/route.ts
@@ -63,7 +63,7 @@ export async function POST(req: Request) {
       })
     );
 
-    const batchSize = 5000;
+    const batchSize = 32000;
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     let batch: any[] = [];
     let verifiedCount = 0;
