7fef6797747e41575f8026885b20f2797d3ba78d
azrhaell <marcelolobo27@gmail.com>
2025-08-06 11:59:15 -0300

    Commit 141.9

--- src/lib/scripts/python/update_listwithbdo.py
+++ src/lib/scripts/python/update_listwithbdo.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
 # Caminhos dos arquivos
-caminho_csv = r'd:\GitHub\definitiveDATABASE\app-database\public\uploads\BDO\bdo_filtrado.csv'
-caminho_xlsx = r'd:\GitHub\definitiveDATABASE\app-database\public\uploads\MISC\CRUZAR BDO - RJ - EMPRESARIOS INDIVIDUAIS.xlsx'
-caminho_saida = r'd:\GitHub\definitiveDATABASE\app-database\public\uploads\MISC\25072025\CRUZAR BDO - RJ - EMPRESARIOS INDIVIDUAIS.xlsx'
+caminho_csv = r'd:\Github\Vivo_Database\app-database\public\BDO\bdo_filtrado.csv'
+caminho_xlsx = r'd:\Github\Vivo_Database\app-database\public\MISC\BASE - PF - INDIVIDUAL - RJ - ES - 05 08 2025.xlsx'
+caminho_saida = r'd:\Github\Vivo_Database\app-database\public\MISC\06082025\BASE - PF - INDIVIDUAL - RJ - ES - 05 08 2025 - SAIDA.xlsx'
 
 # Lê o CSV
 df_csv = pd.read_csv(caminho_csv, dtype=str)
@@ -13,7 +13,7 @@ df_xlsx = pd.read_excel(caminho_xlsx, dtype=str)
 
 # Garante que os campos de comparação estão em formato string e removem espaços
 df_csv['number'] = df_csv['number'].astype(str).str.strip()
-df_xlsx['Telefone Socio'] = df_xlsx['Telefone Socio'].astype(str).str.strip()
+df_xlsx['Telefone_Socio'] = df_xlsx['Telefone_Socio'].astype(str).str.strip()
 
 # Cria um dicionário para mapeamento rápido: número -> (operadora, datahora)
 mapeamento = df_csv.set_index('number')[['operadora', 'datahora']].to_dict(orient='index')
@@ -26,8 +26,8 @@ def atualizar_data(numero):
     return mapeamento[numero]['datahora'] if numero in mapeamento else None
 
 # Atualiza os campos no XLSX
-df_xlsx['Operadora'] = df_xlsx['Telefone Socio'].apply(atualizar_operadora)
-df_xlsx['DATA_ATIVACAO'] = df_xlsx['Telefone Socio'].apply(atualizar_data)
+df_xlsx['OPERADORA'] = df_xlsx['Telefone_Socio'].apply(atualizar_operadora)
+df_xlsx['ATUALIZACAO DATA ATIVACAO'] = df_xlsx['Telefone_Socio'].apply(atualizar_data)
 
 # Salva o resultado
 df_xlsx.to_excel(caminho_saida, index=False)
